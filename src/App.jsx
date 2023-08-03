a4b3698eab6e0e078a74321ab25b5dc1eabeba6b
KALOUSIUS <kalousius@yahoo.com>
2023-08-03 15:24:42 -0800

    Update on the submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import React, { useState } from 'react';
 import GitUserData from './components/GitUserData';
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Import the Font Awesome CSS
-import './index.css'; // Import the CSS filewith Tailwind css style
+import './index.css'; // Import the CSS file with Tailwind css style
 
 function App() {
   const [githubUsername, setGithubUsername] = useState(''); // State to hold the GitHub username
+  const [showProfile, setShowProfile] = useState(false); // State to control profile visibility
 
   const handleInputChange = (event) => {
     setGithubUsername(event.target.value); // Update the state with the typed GitHub username
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowProfile(true); // Set the state to show the user profile
   };
 
   return (
     <div className="App">
       <div className="container">
         <div className="logo-container">
-          <img src=" /github.png" alt="GitHub Logo" className="github-logo" />
+          <img src="/github.png" alt="GitHub Logo" className="github-logo" />
         </div>
         <h1 className="title">Find Github User Profile</h1>
-        <div className="input-container">
-          <input
-            type="text"
-            className="input"
-            placeholder="Enter GitHub username"
-            value={githubUsername}
-            onChange={handleInputChange}
-          />
-        </div>
-        {githubUsername && <GitUserData username={githubUsername} />}
-        <div class="copy-right">
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter GitHub username"
+              value={githubUsername}
+              onChange={handleInputChange}
+            /> <br/> <br/>
+            <button type="submit" className="submit-button">
+              Find
+            </button>
+          </div>
+        </form>
+        {showProfile && <GitUserData username={githubUsername} />}
+        <div className="copy-right">
           &copy; {new Date().getFullYear()} Kalousius. All rights reserved.
         </div>
       </div>
@@ -36,5 +47,3 @@ function App() {
 }
 
 export default App;
-
-
